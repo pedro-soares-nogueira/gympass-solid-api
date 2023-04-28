58da7894884a796fcf6fc5998a1dcfc71eb75713
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-04-28 16:32:05 -0300

    UserAlreadyExistsError

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
+import { UserAlreadyExistsError } from "@/errors/user-already-exists-error"
 import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository"
-import { prisma } from "@/lib/prisma"
 import { RegisterUseCase } from "@/use-cases/register"
-import { hash } from "bcryptjs"
 import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
 
@@ -19,8 +18,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({ name, email, password })
-  } catch (error) {
-    return reply.status(409).send()
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
