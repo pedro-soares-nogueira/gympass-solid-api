de8358241d86a5ceda18414ff8b714bbb1599329
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-05-16 17:27:37 -0300

    getting user lativude

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -2,14 +2,28 @@ import { CheckIn } from "@prisma/client"
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository"
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
 import { CheckInUseCase } from "./check-in"
+import { ImMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository"
+import { Decimal } from "@prisma/client/runtime/library"
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: ImMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe("Check In Use Case", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new ImMemoryGymsRepository()
+
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: "gym-01",
+      title: "JavaScript",
+      description: "JavaScript",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
 
     vi.useFakeTimers()
   })
@@ -22,6 +36,8 @@ describe("Check In Use Case", () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: 21.2259943,
+      userLongitude: 50.4411536,
     })
   })
 
@@ -31,12 +47,16 @@ describe("Check In Use Case", () => {
     await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: 21.2259943,
+      userLongitude: 50.4411536,
     })
 
     await expect(() =>
       sut.execute({
         gymId: "gym-01",
         userId: "user-01",
+        userLatitude: 21.2259943,
+        userLongitude: 50.4411536,
       })
     ).rejects.toBeInstanceOf(Error)
   })
@@ -47,6 +67,8 @@ describe("Check In Use Case", () => {
     await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: 21.2259943,
+      userLongitude: 50.4411536,
     })
 
     vi.setSystemTime(new Date(2023, 0, 21, 8, 0, 0))
@@ -54,6 +76,8 @@ describe("Check In Use Case", () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: 21.2259943,
+      userLongitude: 50.4411536,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,21 +1,34 @@
 import { CheckInsRepository } from "@/repositories/check-ins-repository"
+import { GymsRepository } from "@/repositories/gyms-repository"
 import { CheckIn } from "@prisma/client"
+import { ResourceNotFoundError } from "./errors/resource-not-found-error"
 
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 interface CheckInUseCaseResponse {
   checkIn: CheckIn
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsReposiroty: CheckInsRepository) {}
+  constructor(
+    private checkInsReposiroty: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({
     gymId,
     userId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
     const checkInOnSameDate = await this.checkInsReposiroty.findByUserOnDate(
       userId,
       new Date()
